068a4ef7067a6944937619aff1e7c54654fcb5c5
Sonny Michael <isonnymichael@gmail.com>
2025-06-22 13:48:50 +0700

    feat: change address to mainnet

--- contracts/ignition/modules/PlumePawnModule.ts
+++ contracts/ignition/modules/PlumePawnModule.ts
@@ -1,6 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const PLUME_PUSD_ADDRESS = "0x1E0E030AbCb4f07de629DCCEa458a271e0E82624";
+const PLUME_PUSD_ADDRESS = "0xdddD73F5Df1F0DC31373357beAC77545dC5A6f3F";
 
 const PlumePawnModule = buildModule("PlumePawnModule", (m) => {
 
